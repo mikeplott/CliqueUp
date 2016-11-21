3cdf9dd40f675861c3143dc5d0da10268968aa67
michaelplott <mike.plott80@gmail.com>
2016-11-21 16:39:25 -0500

    Trying to parse message into a hashmap from the socket.

--- src/main/java/com/cliqueup/controllers/WebSocketController.java
+++ src/main/java/com/cliqueup/controllers/WebSocketController.java
@@ -3,8 +3,10 @@ package com.cliqueup.controllers;
 import com.cliqueup.entities.ChatMessage;
 import com.cliqueup.entities.Group;
 import com.cliqueup.entities.User;
+import com.cliqueup.services.ChatMessageRepo;
 import com.cliqueup.services.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.json.JacksonJsonParser;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
@@ -12,6 +14,7 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
+import java.util.LinkedHashMap;
 
 /**
  * Created by michaelplott on 11/19/16.
@@ -28,15 +31,19 @@ public class WebSocketController {
     @Autowired
     UserRepo users;
 
-    @MessageMapping("/global")
-    @SendTo("/global")
-    public Message message(Message message, HttpSession session) {
-        String username = (String) session.getAttribute("username");
-        User user = users.findByUsername(username);
-        String text = (String)message.getPayload();
-        ChatMessage chatMessage = new ChatMessage(text, user);
-        return message;
-    }
+    @Autowired
+    ChatMessageRepo cms;
+
+//    @MessageMapping("/global")
+//    @SendTo("/global")
+//    public Message message(Message message, HttpSession session) {
+//        String username = (String) session.getAttribute("username");
+//        User user = users.findByUsername(username);
+//        String text = (String)message.getPayload();
+//        ChatMessage chatMessage = new ChatMessage(text, user);
+//        cms.save(chatMessage);
+//        return message;
+//    }
 
 //    public String room(ArrayList<Group> groups) {
 //        for (Group group : groups) {
@@ -44,4 +51,24 @@ public class WebSocketController {
 //        }
 //    }
 
+    @MessageMapping("/global")
+    @SendTo("/global")
+    public Message message(Message message) {
+
+        if (new String((byte[]) message.getPayload()).length() > 0) {
+
+
+            LinkedHashMap mapper = new LinkedHashMap();
+            JacksonJsonParser parser = new JacksonJsonParser();
+            mapper = (LinkedHashMap) parser.parseMap(new String((byte[]) message.getPayload()));
+            User user = users.findByUsername((String) mapper.get("username"));
+            String text = (String) mapper.get("message");
+            ChatMessage chatMessage = new ChatMessage(text, user);
+            cms.save(chatMessage);
+            this.messenger.convertAndSend("/global", message);
+        }
+
+        return message;
+
+    }
 }
