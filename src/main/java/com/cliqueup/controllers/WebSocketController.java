51f665bf8ffa3a5d858a47c084d1af0f00332024
michaelplott <mike.plott80@gmail.com>
2016-11-21 13:04:35 -0500

    New Jar built. websocket development starting.

--- src/main/java/com/cliqueup/controllers/WebSocketController.java
+++ src/main/java/com/cliqueup/controllers/WebSocketController.java
@@ -1,11 +1,14 @@
 package com.cliqueup.controllers;
 
+import com.cliqueup.entities.Group;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
 
 /**
  * Created by michaelplott on 11/19/16.
@@ -19,9 +22,10 @@ public class WebSocketController {
         messenger = template;
     }
 
-    @MessageMapping("/message")
-    @SendTo("/message")
+    @MessageMapping("/global")
+    @SendTo("/global")
     public Message message(Message message) {
         return message;
     }
+
 }
