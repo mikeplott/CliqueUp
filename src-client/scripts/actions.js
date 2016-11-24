86897975471eee90e3293c0ce84bebcf42c232e9
Victor Carpenter <victorcarpenter123@gmail.com>
2016-11-24 13:43:05 -0500

    seeing if these tabs work

--- src-client/scripts/actions.js
+++ src-client/scripts/actions.js
@@ -65,73 +65,7 @@ const ACTIONS = {
     })
   },
 
-  connectToSocket: function(){
-
-    let ws = new SockJS("/socket")
-    STORE.setStore('socket', Stomp.over(ws))
-    let socket = STORE.getStoreData()
-    socket = socket.socket
-
-    socket.connect({}, ACTIONS.onSocketConnect)
-
-
-    },
-
-  onSocketConnect: function(){
-    console.log('hey i got here so now what??')
-    let socket = STORE.getStoreData()
-    socket = socket.socket
-
-    socket.subscribe("/global", ACTIONS.onChatConnect   )
-
-
-
-   },
-
-
-   onChatConnect: function(message){
-    //  console.log("hey idk if this will even display cause it didnt last time")
-     console.log(message, message.body)
-     console.log(JSON.parse(message.body))
-   },
-
-   onReceivedMessage: function(message){
-     let data = JSON.parse(message.body)
-
-     let textBlock = (
-        <div>
-          <div>
-            <img src="http://facebookcraze.com/wp-content/uploads/2010/10/fake-facebook-profile-picture-funny-batman-pic.jpg"/>
-          </div>
-          <div>
-            <h4>{data.message}</h4>
-          </div>
-          <p>12:00</p>
-        </div>
-     )
-
-
-   },
-
-   sendMessage: function(chtMess){
-     let socket = STORE.getStoreData()
-     console.log(socket)
-     let user = socket.loginData
-     socket = socket.socket
-
-     let theMess = {
-       message: chtMess,
-       username: user
-     }
-
-     socket.send('/global', {} ,JSON.stringify(theMess))
-
-
-
-
-
-   },
-
+  
 
   handleUserLogin: function(usrInfo){
     theCrntUser = usrInfo
