0c10cb6d944fdda0e2e8d9c94304349202a22d9c
Victor Carpenter <victorcarpenter123@gmail.com>
2016-12-01 13:50:13 -0500

    push

--- src-client/scripts/Button-self.js
+++ src-client/scripts/Button-self.js
@@ -73,7 +73,7 @@ class SelfView extends React.Component{
 
 
            <a className="list-group-item"  key={i} data-index={markerIndex} onClick={throwMarker}>
-              <div className="eventPicHolder" data-index={markerIndex}><img data-index={markerIndex} src="http://facebookcraze.com/wp-content/uploads/2010/10/fake-facebook-profile-picture-funny-batman-pic.jpg" className="eventPics"/></div>
+              <div className="eventPicHolder" data-index={markerIndex}></div>
               <div className="eventDetailHolder" data-index={markerIndex}>
                 <h4 className="list-group-item-heading" data-index={markerIndex}>{element.name}</h4>
                 <p className="list-group-item-text" data-index={markerIndex}>{element.group.name}</p>
